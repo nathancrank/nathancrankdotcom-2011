baf0e84357fbef38552496d418594ca6eac5e1bd
Nathan Crank <github@nathancrank.com>
2011-09-27 00:55:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nathancrankdotcom::Application.config.secret_token = '5a5b8aa2290ad8e5794cb666cb13a487152887c143dec785acdc79fc0535ad01fda1d13bed002b4fce934b10c1c92a04f7cdf6f1c9d5b397311f1fad4e8092b9'
