9f680f9bd2adfbe02c426b0122c3a099b45f13b0
Nathan Crank <github@nathancrank.com>
2011-09-30 14:06:33 -0400

    Completed static pages, began rspec dev

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def portfolio
+    @title = "portfolio"
   end
 
   def blog
+    @title = "blog"
   end
 
   def contact
+    @title = "contact"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,25 +1,50 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
+#Test Portfolio Page
   describe "GET 'portfolio'" do
+    #Test existence
     it "should be successful" do
       get 'portfolio'
       response.should be_success
     end
+    #Test title
+    it "should have the right title" do
+      get 'portfolio'
+      response.should have_selector("title",
+                        :content => ".portfolio")
+    end
   end
 
+#Test Blog page
   describe "GET 'blog'" do
+    #Test existence
     it "should be successful" do
       get 'blog'
       response.should be_success
     end
+    #Test title
+    it "should have the right title" do
+      get 'blog'
+      response.should have_selector("title",
+                        :content => ".blog")
+    end
   end
 
+#Test Contact Page
   describe "GET 'contact'" do
+    #Test existence
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    #Test title
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => ".contact")
     end
   end
 
