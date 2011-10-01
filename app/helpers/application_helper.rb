cb89f1d9aa03168f7257eec5aad993ced228ab47
Nathan Crank <github@nathancrank.com>
2011-09-30 21:44:38 -0400

    Lots of changes, basic CSS added, far from complete... look great in safari and chrome, not quite right on firefox, but not terrible. iPhone is meh at best so far. Media queries have been added in in only the most basic ways, changes width. Havent even touched ie. wont focus on anything other than safari until that it feature complete, then adjust by browser. no ruby of database has really been added. Lots of SASS CSS has been however. Looking pretty decent so far.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  #return a title page on a per-page basis
+  def title
+    base_title = "nathancrank"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title}.#{@title}"
+    end
+  end
+  
+  
 end
