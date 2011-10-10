1b5eaac9d69524abbf254e377f7c48b9002506fb
Nathan Crank <github@nathancrank.com>
2011-10-10 16:05:46 -0400

    maybe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,6 @@ Nathancrankdotcom::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  Sass::Plugin.options[:never_update] = true
+  
 end
+Sass::Plugin.options[:never_update] = true
