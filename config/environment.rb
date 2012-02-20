ec45777c6028aae3f28a8bcea2e2ae019157decc
Nathan Crank <github@nathancrank.com>
2012-02-19 23:23:17 -0500

    changes to resume and rack::cache removed
    - rack:cache removed since its causing issues right now in development,
    its really starting to bug me, ill come back to it in the future
    - resume changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'rack-cache'
-
 # Initialize the rails application
 Nathancrankdotcom::Application.initialize!
